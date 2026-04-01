a209ec864018226091872d41df45e56d8ab32a17
jeonseungwoo735 <jeonsweungwoo735@goodle.com>
2026-04-01 21:19:26 +0900

    bigdata

--- 05_플라스크/app.py
+++ 05_플라스크/app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/Hello")
+def hello():
+    return("만나서 반갑습니다.")
+
+@app.route("/user/<userid>")
+def profile(userid):
+    return f"{userid}\' profile"
+
+if __name__ == "__main__":
+    app.run()
